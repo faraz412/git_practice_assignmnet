884a3bf7ddfee5adf0f1575104f833465cb5847d
faraz412 <ahmedfaraz412@gmail.com>
2022-09-20 23:57:50 +0530

    code fixed by m1

--- prime.js
+++ prime.js
@@ -1,12 +1,12 @@
-let n = 10;
-for (i = 1; i <= n; i++) {
-  let count = 0;
-  for (j = 1; j <= i; j++) {
+let num = 10;
+for (i = 1; i <= num; i++) {
+  let factors = 0;
+  for (j = 1; j <= num; j++) {
     if (i % j == 0) {
-      count++;
+      factors++;
     }
   }
-  if (count == 2) {
+  if (factors == 2) {
     console.log(i, "Prime");
   } else {
     console.log(i, "Not Prime");
